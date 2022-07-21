229534582272942f282071c0676473388d48faed
Алиса <petrunina.alisa@gmail.com>
2022-07-22 00:03:44 +0300

    69

--- Домашнее задание/Program1.cs
+++ Домашнее задание/Program1.cs
@@ -5,4 +5,17 @@
 //Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 //A = 3; B = 5 -> 243 (3⁵)
 //A = 2; B = 3 -> 8
-
+/*
+int PowNumbers(int A, int B)
+{
+    if (B == 0) return 1;
+    if (B == 1) return A;
+    else return PowNumbers(A, B - 1) * A;
+}
+Console.Clear();
+Console.WriteLine("Input A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PowNumbers(A, B));
+*/
